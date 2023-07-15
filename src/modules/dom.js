191bb6bef07c43b34ed128028082ea93148002c7
Tyler Pardee <tjpardee@gmail.com>
2023-07-15 15:36:55 -0400

    Fix Expand Functionality

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,10 +1,17 @@
 function addListeners() {
   const events = document.querySelectorAll('.expand');
   events.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-      btn.nextElementSibling.classList.toggle('show');
-    });
+    btn.addEventListener('click', toggle.bind(null, btn));
   });
+}
+
+function toggle(btn) {
+  btn.nextElementSibling.classList.toggle('show');
+  if (btn.innerHTML === '▼') {
+    btn.innerHTML = '▲';
+  } else {
+    btn.innerHTML = '▼';
+  }
 }
 
 export default addListeners;
